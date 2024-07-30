4101ad94a7405d1473a9e4e5d39e114fbe4fea7b
Khachik Astoyan <kastoyan@griddynamics.com>
2024-07-30 23:16:50 +0400

    add buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
+const Button = (props) => {
+  return (
+    <button
+      className="bg-blue-50 py-3 px-7 rounded-md hover:bg-blue-500 transition-colors hover:text-white"
+      {...props}
+    />
+  );
+};
+
 function App() {
-  return "Hello world";
+  const [token, setToken] = useState(null);
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center gap-3 pt-16">
+      {!token && (
+        <>
+          <Button onClick={() => setToken("blah")}>Log in</Button>
+          <Button>Sign up</Button>
+          <Button>View public file</Button>
+        </>
+      )}
+
+      {token && (
+        <>
+          <Button onClick={() => setToken(null)}>Log out</Button>
+          <Button>View public file</Button>
+          <Button>View private file</Button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
