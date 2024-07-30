cbbaa536b78f6bf5adde4ff81e279e91c0c2aaee
Khachik Astoyan <kastoyan@griddynamics.com>
2024-07-30 23:38:19 +0400

    mock user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,23 +10,38 @@ const Button = (props) => {
 };
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center gap-3 pt-16">
-      {!token && (
+    <div className="w-screen flex justify-center items-center gap-3 pt-16">
+      {!user && (
         <>
-          <Button onClick={() => setToken("blah")}>Log in</Button>
+          <Button
+            onClick={() =>
+              setUser({
+                name: "John Doe",
+              })
+            }
+          >
+            Log in
+          </Button>
           <Button>Sign up</Button>
           <Button>View public file</Button>
         </>
       )}
 
-      {token && (
+      {user && (
         <>
-          <Button onClick={() => setToken(null)}>Log out</Button>
-          <Button>View public file</Button>
-          <Button>View private file</Button>
+          <div>
+            <h1 className="mb-4 text-center text-xl font-bold">
+              Welcome, {user.name}
+            </h1>
+            <div className="flex gap-3">
+              <Button onClick={() => setUser(null)}>Log out</Button>
+              <Button>View public file</Button>
+              <Button>View private file</Button>
+            </div>
+          </div>
         </>
       )}
     </div>
